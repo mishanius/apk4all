44883f143fa6b54135d9dc1716b2d5317cef1c03
mishanius <michael3120@gmail.com>
2017-08-11 13:02:01 +0300

    added top 15 to main

--- apk_world/static/pages/js/angular/controllers/mainCtrl.js
+++ apk_world/static/pages/js/angular/controllers/mainCtrl.js
@@ -1,8 +1,8 @@
 angular.module('apk_world_app')
 	.controller('mainCtrl', function mainCtrl($scope, $location) {
 	    $scope.media = media;
-	    $scope.TopGames = topgames
-	    $scope.TopApps = topapps
+	    $scope.TopGames = topgames;
+	    $scope.TopApps = topapps;
 	    $scope.users = ['Fabio', 'Leonardo', 'Thomas', 'Gabriele', 'Fabrizio', 'John', 'Luis', 'Kate', 'Max'];
 	    $scope.search = function(kw){
                                     $location.path("/search"+kw)
